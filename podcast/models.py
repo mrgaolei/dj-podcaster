b9c017505bf338ee2edb402f4fc0e76501141743
mrgaolei <gaolei@liheran.com>
2013-06-21 16:18:38 +0800

    init

---
+++ podcast/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Feed(models.Model):
+	pass
+
+class Tag(models.Model):
+	pass
+
+class Podcast(models.Model):
+	pass
