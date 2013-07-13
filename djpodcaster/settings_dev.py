0aa7211c0328ee235bf86fad9198dacab2d89fa6
mrgaolei <gaolei@liheran.com>
2013-07-13 16:01:09 +0800

    线上配置分离

--- djpodcaster/settings_dev.py
+++ djpodcaster/settings_dev.py
@@ -19,6 +19,14 @@ DATABASES = {
         'PASSWORD': '',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
+    },
+    'tangsuanradio': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tangsuanradio',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
