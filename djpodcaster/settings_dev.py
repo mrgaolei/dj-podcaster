1193dc0e169e8b45d7f739307c717f03d0e66925
mrgaolei <gaolei@liheran.com>
2015-07-12 21:20:44 +0800

    add static_root

--- djpodcaster/settings_dev.py
+++ djpodcaster/settings_dev.py
@@ -66,7 +66,7 @@ MEDIA_URL = 'http://media.ts.com/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(os.path.dirname(__file__), '../static').replace('\\','/')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
