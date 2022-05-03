6d9869f89880c184a7fad5c2be2fbdbdd29ce197
GL <guafeng@fmit.cn>
2022-05-04 01:11:03 +0800

    support for django 3.2

--- podcast/views.py
+++ podcast/views.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from django.shortcuts import render, render_to_response, redirect, get_list_or_404, get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from podcast.models import Feed, Podcast
 
 def home(request):
@@ -8,4 +8,4 @@ def home(request):
 def showfeed(request, filename):
 	feed = get_object_or_404(Feed, filename=filename)
 	podcasts = Podcast.objects.filter(feeds=feed)
-	return render_to_response("podcast/feed.html", {'feed':feed,'podcasts':podcasts,'now':datetime.now()})
+	return render(request, "podcast/feed.html", {'feed':feed,'podcasts':podcasts,'now':datetime.now()})
